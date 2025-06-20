cee4cf29eea38d82f73ddbdee84b21bb4012d9aa
Brahmjot Singh <brahmjots111@gmail.com>
2025-06-20 22:59:47 +0530

    fix backend api

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || "https://frequent-6ig0.onrender.com:5000/api";
+export const API_URL = import.meta.env.VITE_API_URL || "https://frequent-6ig0.onrender.com/api";
